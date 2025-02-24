691783db4096b602819899252ffb75889f8c950d
Kaloyan Kolev <kkolev05@gmail.com>
2025-02-24 17:15:09 +0200

    Removed the /startattack function; moved to a separate addon

--- TeronAPI.lua
+++ TeronAPI.lua
@@ -1,28 +1,4 @@
 --General custom APIs
-    --AttackTarget - Version 2
-    local attacking;
-    local target_change;
-    local no_combat;
-    local f = CreateFrame'Frame';
-
-    no_combat = PlayerFrame.inCombat;
-
-    f:RegisterEvent'PLAYER_ENTER_COMBAT'
-    f:RegisterEvent'PLAYER_LEAVE_COMBAT'
-    f:SetScript('OnEvent', function()
-        attacking = event == 'PLAYER_ENTER_COMBAT'
-        target_change = event == 'PLAYER_TARGET_CHANGED'
-    end)
-    
-    SLASH_ATTACK1 = '/startattack'
-    function SlashCmdList.ATTACK(command)
-        if not attacking or target_change or no_combat then
-            AttackTarget();
-            --TargetNearestEnemy();
-            --CastSpellByName'Attack'
-        end
-    end
-
     --f:SetScript('OnUpdate', function()
         --f:SetScript('OnUpdate', nil)
         --for i = 1, GetNumMacros() do
@@ -92,6 +68,20 @@
     --end
 
 --Warrior custom APIs
+    --Global variables for auto attack function
+    local attacking;
+    local target_change;
+    local no_combat;
+    local f = CreateFrame'Frame';
+
+    no_combat = PlayerFrame.inCombat;
+
+    f:RegisterEvent'PLAYER_ENTER_COMBAT'
+    f:RegisterEvent'PLAYER_LEAVE_COMBAT'
+    f:SetScript('OnEvent', function()
+        attacking = event == 'PLAYER_ENTER_COMBAT'
+        target_change = event == 'PLAYER_TARGET_CHANGED'
+    end)
     --API for warrior range pull spell
     function WarriorRangePull()
         local _,_,i=strfind(GetInventoryItemLink("player",18),"\124Hitem:(%d+)");
